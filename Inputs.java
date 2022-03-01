ea937d83c30eb42fe78cfce1d6524b86d5d83dd2
adnan-iotexcel <93937216+SDE-ADNAN@users.noreply.github.com>
2022-03-02 02:21:01 +0530

    added and practised input output and addition of two numbers in java

---
+++ Inputs.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        // // System.out.println(input.next());
+
+        // int rollno = input.nextInt();
+
+        // System.out.println("ur roll no is " + rollno);
+
+        // int a = 10; // 10 is literal for var a 
+        // System.out.println(a);
+
+        // int b = 234_000_000;
+        // System.out.println(b);
+
+
+        // String  name = input.nextLine();
+        // System.out.println(name);
+
+
+        // Float  floatnum = input.nextFloat();
+        // System.out.println(floatnum);
+    }
+}
---
+++ Primitives.java
@@ -0,0 +1,39 @@
+public class Primitives {
+    public static void main(String[] args) {
+        //Primitive DataTypes
+        //Primitive datatypes are the data types which could not be divided further in to more pieces 
+        // e.g string can be divided into individual char  so it is a non 
+        // primitive datatype but char is a primitive datatype
+
+        //String is a non primitive datatype
+
+        String name = "Adnan";
+        System.out.println(name);
+
+        //Some primitive datatypes 
+
+        char character = 'A';
+        System.out.println(character);
+
+        int number = 64;
+        System.out.println(number);
+
+        float DecimalNumbers = 4.5f;
+        System.out.println(DecimalNumbers);
+
+        double LongDecimalNumbers = 45454.45454;
+        System.out.println(LongDecimalNumbers);
+
+        double LongIntegerNumbers = 343434343434343434l;
+        System.out.println(LongIntegerNumbers);
+
+        boolean check = true;
+        System.out.println(check);
+
+
+
+
+        
+        
+    }
+}
