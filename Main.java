b687d896f4fef4d374673a2f501731378eeaba00
adnan-iotexcel <93937216+SDE-ADNAN@users.noreply.github.com>
2022-03-02 00:45:48 +0530

    done some input and scanning input

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
 // import javax.sound.sampled.SourceDataLine;
 
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Helloworld");
+// public class Main{
+//     public static void main(String[] args){
+//         // System.out.println(args[1]);
+//         System.out.println("Hello World !!!!!!!!!!");
+//     }
+
+
+// }
+
+// package com.adnan;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main (String[] args) {
+	// write your code here
+//        System.out.println("hello");
+        Scanner input = new Scanner(System.in);
+        // System.out.println(input.nextInt());   // for int value in input
+        // System.out.println(input.nextLine());     // for full line in input
+        System.out.println(input.next());      // for  all data type datatype
     }
-
-
 }
